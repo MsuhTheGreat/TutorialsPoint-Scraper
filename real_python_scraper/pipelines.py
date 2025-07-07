0ac0356386303b4ae2367cb3495530e90a96125f
MsuhTheGreat <m.usmantwpsc@gmail.com>
2025-07-07 05:07:59 +0500

    saving...

--- real_python_scraper/pipelines.py
+++ real_python_scraper/pipelines.py
@@ -10,4 +10,30 @@ from itemadapter import ItemAdapter
 
 class RealPythonPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        # Strip Whitespaces
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            value = adapter.get(field_name)
+            if isinstance(value, str):
+                adapter[field_name] = value.strip()
+        
+        # Get correct publish date
+        date_lst = adapter.get("publish_date")
+        adapter["publish_date"] = date_lst[-1]
+
+        # Convert List of contents to a string
+        list_of_contents = adapter.get("list_of_contents")
+        value = "\n".join(list_of_contents)
+        adapter["list_of_contents"] = value
+
+        # Convert tags to a string
+        tags_lst = adapter.get("tags")
+        if len(tags_lst) == 1:
+            value = tags_lst[0]
+        else:
+            value = ", ".join(tags_lst)
+        adapter["tags"] = value
+
         return item
--- real_python_scraper/spiders/tutorials.py
+++ real_python_scraper/spiders/tutorials.py
@@ -1,5 +1,6 @@
 import scrapy
 from real_python_scraper.items import RealPythonItem
+from trafilatura import extract_metadata
 
 
 class TutorialsSpider(scrapy.Spider):
@@ -12,8 +13,7 @@ class TutorialsSpider(scrapy.Spider):
         'title': '//div[contains(@class, "col-md-11") or contains(@class, "col-lg-8") or contains(@class, "article") or contains(@class, "with-headerlinks")]//h1/text()',
         'author': '//div[contains(@class, "mb-0")]/span[contains(@class, "text-muted")]/a[contains(@class, "text-muted") and contains(@href, "#author")]/text()',
         'tags': '//span[contains(@class, "d-inline") or contains(@class, "d-md-block")]/a[contains(@class, "badge") or contains(@class, "badge-light") or contains(@class, "text-muted")]/text()',
-        'list_of_contents': '//div[contains(@class, "toc")]/ul/li/a/text()',
-        'summary': '//div[contains(@class, "article-body")]//p[1]',
+        'list_of_contents': '//div[contains(@class, "article-body")]//div[contains(@class, "toc")]/ul/li',
         'publish_date': '//div[contains(@class, "mb-0")]//span[contains(@class, "text-muted")]/text()[normalize-space()]',
         'next_page': '//ul[contains(@class, "pagination")]/li[@class="page-item"][last()]/a/@href'
     }
@@ -43,10 +43,32 @@ class TutorialsSpider(scrapy.Spider):
         real_python_item["title"]            = response.xpath(self.selectors["title"]).get(default=None)
         real_python_item["author"]           = response.xpath(self.selectors["author"]).get(default=None)
         real_python_item["publish_date"]     = response.xpath(self.selectors["publish_date"]).getall()
-        real_python_item["summary"]          = response.xpath(self.selectors["summary"]).xpath(".//text() | .//code/text()").getall()
-        real_python_item["list_of_contents"] = response.xpath(self.selectors["list_of_contents"]).getall()
+        real_python_item["summary"]          = self.get_summary(response)
+        real_python_item["list_of_contents"] = self.list_of_contents(response, self.selectors["list_of_contents"])
         real_python_item["tags"]             = response.xpath(self.selectors["tags"]).getall()
         real_python_item["url"]              = response.url
 
         yield real_python_item
     
+
+    def list_of_contents(self, response, li_elements_selector):
+        lst = []
+        li_elements = response.xpath(li_elements_selector)
+        for li_element in li_elements:
+            lst.append(li_element.xpath('/a/text()').get(default=None))
+            if li_element.xpath('/ul'):
+                sub_li_elements = li_element.xpath('/ul/li')
+                for sub_li_element in sub_li_elements:
+                    result = sub_li_element.xpath('/a/text()').get(default=None)
+                    if result:
+                        lst.append(f"\t{result}")
+        return lst
+    
+
+    def get_summary(self, response):
+        html = response.text
+        metadata = extract_metadata(html)
+        if metadata and metadata.description:
+            return metadata.description.strip()
+        return None
+
